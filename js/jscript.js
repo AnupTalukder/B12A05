67efc34a6e4370445ba0d3e0636190931cdb7609
AnupTalukder <anuptalukder445@gmail.com>
2025-09-04 22:40:34 +0600

    love and coin count

---
+++ js/jscript.js
@@ -0,0 +1,52 @@
+
+function callPop(card) {
+    const paragraph = card.querySelector("p").innerText;
+    const number = card.querySelector("h2").innerText;
+
+    alert("Calling " + paragraph + " Service" + " : " + number);
+}
+
+document.getElementById("National-Emergency").addEventListener("click", function (e) {
+    
+    const card = this.closest(".card");
+    callPop(card);
+})
+
+
+
+// Love Count 
+
+const icons = document.querySelectorAll('.icon');
+const counter = document.getElementById('love-count');
+let Love_count = 0;
+
+icons.forEach(icon => {
+    icon.addEventListener('click', function () {
+        Love_count++;
+        counter.textContent = Love_count;
+    });
+});
+
+
+//coin count 
+
+
+const callButtons = document.querySelectorAll('.calls');
+const coinCountEl = document.getElementById('coin-count');
+let coins = parseInt(coinCountEl.textContent);
+
+callButtons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        if (coins >= 20) {
+            coins = coins-20;
+            coinCountEl.textContent = coins;
+        } else {
+            alert("Not enough coins!");
+        }
+    });
+});
+
+
+
+
+
